007f2c10f7af19a7ce297ca69917331adf9096a1
Titan antony <antonytitan96@gmail.com>
2024-11-19 15:44:19 +0300

    Chore: learning call back functions

--- fs.js
+++ fs.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
 
-fs.readFile('sampletext.txt', 'utf8', (err, data) =>{
+fs.readFile('sampltext.txt', 'utf8', (err, data) =>{
     if (err) {
-        console.log(err);
+        console.log("Error reading the file", err);
         return;
     }
-    console.log(data);
+    console.log("File content", data);
 })
+
+console.log("This message prints while the file is being read.")
+
